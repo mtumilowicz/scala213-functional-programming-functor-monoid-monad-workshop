c0583b1caedbbef2195368f1e753a708cfbe8e0f
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-07-29 21:20:23 +0200

    Monad.scala draft

--- src/main/scala/common/Monad.scala
+++ src/main/scala/common/Monad.scala
@@ -4,4 +4,7 @@ trait Monad[F[_]] extends Functor[F] {
   def unit[A](a: => A): F[A]
 
   def flatMap[A, B](ma: F[A])(f: A => F[B]): F[B]
+
+//  def sequence[A](lma: List[F[A]]): F[List[A]]
+//  def traverse[A,B](la: List[A])(f: A => F[B]): F[List[B]]
 }
