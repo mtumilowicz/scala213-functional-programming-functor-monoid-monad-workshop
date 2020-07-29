15966659425c8bee83149aabe0c5b064a2df046b
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-07-29 11:05:24 +0200

    MonoidTest.scala

--- src/test/scala/common/MonoidTest.scala
+++ src/test/scala/common/MonoidTest.scala
@@ -1,18 +1,13 @@
 package common
 
-import org.scalatest._
+class MonoidTest extends org.scalatest.FunSuite with org.scalatest.matchers.should.Matchers {
 
-class MonoidTest extends propspec.AnyPropSpec
-  with org.scalatest.matchers.should.Matchers {
+  test("ListConcat") {
+    def listConcat[A]: Monoid[List[A]] = new Monoid[List[A]] {
+      override def op(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
 
-  property("ListConcat") {
-    def ListConcat[A]: Monoid[List[A]] = new Monoid[List[A]] {
-      def op(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
-
-      val zero: List[Nothing] = Nil
+      override val zero: List[Nothing] = Nil
     }
-
-    def listConcat: Monoid[List[String]] = ListConcat[String]
 
     listConcat.op(List("a"), List("b")) shouldBe List("a", "b")
     listConcat.op(listConcat.zero, List("b")) shouldBe List("b")
@@ -20,14 +15,12 @@ class MonoidTest extends propspec.AnyPropSpec
     listConcat.zero shouldBe Nil
   }
 
-  property("IntSum") {
-    def IntSum: Monoid[Int] = new Monoid[Int] {
+  test("IntSum") {
+    def intSum: Monoid[Int] = new Monoid[Int] {
       override def op(a1: Int, a2: Int): Int = a1 + a2
 
       override def zero: Int = 0
     }
-
-    def intSum: Monoid[Int] = IntSum
 
     intSum.op(2, 3) shouldBe 5
     intSum.op(intSum.zero, 3) shouldBe 3
@@ -35,14 +28,12 @@ class MonoidTest extends propspec.AnyPropSpec
     intSum.zero shouldBe 0
   }
 
-  property("IntMultiplication") {
-    def IntMultiplication: Monoid[Int] = new Monoid[Int] {
+  test("IntMultiplication") {
+    def intMultiplication: Monoid[Int] = new Monoid[Int] {
       override def op(a1: Int, a2: Int): Int = a1 * a2
 
       override def zero: Int = 1
     }
-
-    def intMultiplication: Monoid[Int] = IntMultiplication
 
     intMultiplication.op(2, 3) shouldBe 6
     intMultiplication.op(intMultiplication.zero, 3) shouldBe 3
