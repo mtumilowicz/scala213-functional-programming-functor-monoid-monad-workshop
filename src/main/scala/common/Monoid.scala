fcbfc9a417f0bfebaee2dc94cb7633dedb1034e6
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-07-29 10:15:59 +0200

    Monoid draft

---
+++ src/main/scala/common/Monoid.scala
@@ -0,0 +1,7 @@
+package common
+
+trait Monoid[A] {
+  def op(a1: A, a2: A): A
+
+  def zero: A
+}
---
+++ src/test/scala/common/MonoidTest.scala
@@ -0,0 +1,50 @@
+package common
+
+class MonoidTest extends org.scalatest.FunSuite
+  with org.scalatest.matchers.should.Matchers {
+
+  test("ListConcat") {
+    def ListConcat[A]: Monoid[List[A]] = new Monoid[List[A]] {
+      def op(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
+
+      val zero: List[Nothing] = Nil
+    }
+
+    def listConcat: Monoid[List[String]] = ListConcat[String]
+
+    listConcat.op(List("a"), List("b")) shouldBe List("a", "b")
+    listConcat.op(listConcat.zero, List("b")) shouldBe List("b")
+    listConcat.op(List("a"), listConcat.zero) shouldBe List("a")
+    listConcat.zero shouldBe Nil
+  }
+
+  test("IntSum") {
+    def IntSum: Monoid[Int] = new Monoid[Int] {
+      override def op(a1: Int, a2: Int): Int = a1 + a2
+
+      override def zero: Int = 0
+    }
+
+    def intSum: Monoid[Int] = IntSum
+
+    intSum.op(2, 3) shouldBe 5
+    intSum.op(intSum.zero, 3) shouldBe 3
+    intSum.op(2, intSum.zero) shouldBe 2
+    intSum.zero shouldBe 0
+  }
+
+  test("IntMultiplication") {
+    def IntMultiplication: Monoid[Int] = new Monoid[Int] {
+      override def op(a1: Int, a2: Int): Int = a1 * a2
+
+      override def zero: Int = 1
+    }
+
+    def intMultiplication: Monoid[Int] = IntMultiplication
+
+    intMultiplication.op(2, 3) shouldBe 6
+    intMultiplication.op(intMultiplication.zero, 3) shouldBe 3
+    intMultiplication.op(2, intMultiplication.zero) shouldBe 2
+    intMultiplication.zero shouldBe 1
+  }
+}
