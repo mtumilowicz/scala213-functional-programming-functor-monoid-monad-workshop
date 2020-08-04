ce70c5f0dd2a3047e12d83230ea59d249d2a4831
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-08-04 20:59:40 +0200

    draft

--- src/main/scala/common/Applicative.scala
+++ src/main/scala/common/Applicative.scala
@@ -5,9 +5,12 @@ trait Applicative[F[_]] extends Functor[F] {
 
   def unit[A](a: => A): F[A]
 
-  def map[B](fa: F[A])(f: A => B): F[B] =
+  def map[A, B](fa: F[A])(f: A => B): F[B] =
     map2(fa, unit(()))((a, _) => f(a))
 
   def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] =
     as.foldRight(unit(List[B]()))((a, fbs) => map2(f(a), fbs)(_ :: _))
+
+  def sequence[A](lfa: List[F[A]]): F[List[A]] =
+    traverse(lfa)(fa => fa)
 }
--- src/main/scala/common/Monad.scala
+++ src/main/scala/common/Monad.scala
@@ -1,22 +1,11 @@
 package common
 
-import answers.IO
-
-trait Monad[F[_]] extends Functor[F] {
+trait Monad[F[_]] extends Applicative[F] {
 
   def unit[A](a: => A): F[A]
 
-  def map[A,B](ma: F[A])(f: A => B): F[B] =
-    flatMap(ma)(a => unit(f(a)))
+  def flatMap[A, B](ma: F[A])(f: A => F[B]): F[B]
 
   def map2[A,B,C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] =
     flatMap(ma)(a => map(mb)(b => f(a, b)))
-
-  def flatMap[A, B](ma: F[A])(f: A => F[B]): F[B]
-
-  def sequence[A](lfa: List[F[A]]): F[List[A]] =
-  traverse(lfa)(fa => fa)
-
-  def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] =
-  as.foldRight(unit(List[B]()))((a, mbs) => map2(f(a), mbs)(_ :: _))
 }
