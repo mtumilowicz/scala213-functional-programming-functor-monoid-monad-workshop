aa16945ab5cb5079f3a67284af8c9fe628e4a086
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-08-04 21:07:18 +0200

    draft

--- src/main/scala/common/Applicative.scala
+++ src/main/scala/common/Applicative.scala
@@ -1,9 +1,13 @@
 package common
 
 trait Applicative[F[_]] extends Functor[F] {
-  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
+  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    apply(map(fa)(f.curried))(fb)
 
   def unit[A](a: => A): F[A]
+
+  def apply[A,B](fab: F[A => B])(fa: F[A]): F[B] =
+    map2(fab, fa)(_(_))
 
   def map[A, B](fa: F[A])(f: A => B): F[B] =
     map2(fa, unit(()))((a, _) => f(a))
@@ -13,4 +17,30 @@ trait Applicative[F[_]] extends Functor[F] {
 
   def sequence[A](lfa: List[F[A]]): F[List[A]] =
     traverse(lfa)(fa => fa)
+}
+
+object Applicative {
+  def validationApplicative[E]: Applicative[({type f[x] = Validation[E, x]})#f] =
+    new Applicative[({type f[x] = Validation[E, x]})#f] {
+      def unit[A](a: => A): Success[A] = Success(a)
+
+      override def map2[A, B, C](fa: Validation[E, A], fb: Validation[E, B])(f: (A, B) => C): Validation[E, C] =
+        (fa, fb) match {
+          case (Success(a), Success(b)) => Success(f(a, b))
+          case (Failure(h1, t1), Failure(h2, t2)) =>
+            Failure(h1, t1 ++ Vector(h2) ++ t2)
+          case (e@Failure(_, _), _) => e
+          case (_, e@Failure(_, _)) => e
+        }
+    }
+
+  type Const[A, B] = A
+
+  implicit def monoidApplicative[M](M: Monoid[M]): Applicative[({type f[x] = Const[M, x]})#f] =
+    new Applicative[({type f[x] = Const[M, x]})#f] {
+      def unit[A](a: => A): M = M.zero
+
+      override def apply[A, B](m1: M)(m2: M): M = M.op(m1, m2)
+    }
+
 }
---
+++ src/main/scala/common/Validation.scala
@@ -0,0 +1,8 @@
+package common
+
+sealed trait Validation[+E, +A]
+
+case class Failure[E](head: E, tail: Vector[E])
+  extends Validation[E, Nothing]
+
+case class Success[A](a: A) extends Validation[Nothing, A]
