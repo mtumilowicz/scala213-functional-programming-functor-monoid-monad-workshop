87cefa1125312fd9c8b8ac55e643f0d71e4e89e8
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-11-20 20:51:06 +0100

    cleaning

--- src/main/scala/structures/validation/Name.scala
+++ src/main/scala/structures/validation/Name.scala
@@ -1,5 +1,11 @@
 package structures.validation
 
-case class Name(name: String) {
-  require(name != null)
+import structures.{Failure, Success, Validation}
+
+case class Name private(name: String)
+
+object Name {
+  def check(name: String): Validation[String, Name] =
+    if (name != "") Success(Name(name))
+    else Failure("Name cannot be empty")
 }
--- src/main/scala/structures/validation/PhoneNumber.scala
+++ src/main/scala/structures/validation/PhoneNumber.scala
@@ -1,5 +1,12 @@
 package structures.validation
 
-case class PhoneNumber(raw: String) {
-  require(raw.matches("[0-9]{10}"))
+import structures.{Failure, Success, Validation}
+
+case class PhoneNumber private(raw: String)
+
+object PhoneNumber {
+  def check(phoneNumber: String): Validation[String, PhoneNumber] =
+    if (phoneNumber.matches("[0-9]{10}"))
+      Success(PhoneNumber(phoneNumber))
+    else Failure("Phone number must be 10 digits")
 }
