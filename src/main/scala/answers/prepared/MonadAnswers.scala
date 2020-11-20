070044caf947d365acee87fad40b79486dc2d07b
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-11-20 10:24:22 +0100

    MonadAnswers.scala

---
+++ src/main/scala/answers/prepared/MonadAnswers.scala
@@ -0,0 +1,15 @@
+package answers.prepared
+
+import common.Monad
+
+object MonadAnswers {
+  def maybe: Monad[Option] = new Monad[Option] {
+    override def unit[A](a: => A): Option[A] = Some(a)
+
+    override def flatMap[A, B](ma: Option[A])(f: A => Option[B]): Option[B] =
+      ma match {
+        case Some(value) => f(value)
+        case None => None
+      }
+  }
+}
