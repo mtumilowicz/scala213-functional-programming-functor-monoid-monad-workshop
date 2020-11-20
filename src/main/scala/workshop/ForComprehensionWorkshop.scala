9718eb0af64c29df9e51ee4e8dce701c606c2ee7
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-11-20 21:31:21 +0100

    ForComprehensionWorkshop

---
+++ src/main/scala/workshop/ForComprehensionWorkshop.scala
@@ -0,0 +1,12 @@
+package workshop
+
+object ForComprehensionWorkshop {
+
+  def evenCombinations(ints: List[Int], chars: List[Char], strings: List[String]): List[String] = null
+  // for comprehension
+  // ints if even
+  // chars
+  // strings
+  // yield, string interpolation
+
+}
